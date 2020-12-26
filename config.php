49d70c2a6ad19f6ede849f498100770ab96c47c7
Rayene Limem <rayane.limem@gmail.com>
2020-12-26 12:54:30 +0100

    somme

--- config.php
+++ config.php
@@ -3,3 +3,4 @@
 define("INPUT_DATA") = serialize(array(1=>1,3=>4,10=>12));
 
 
+?>
--- tab.php
+++ tab.php
@@ -1,12 +1,12 @@
 
 <?php
   
-$tab_caracteristique_dupont = array("prénom" => "PAUL","profession" =>
-"ministre","age" => 50);
-$tab_caracteristique_durand = array("prénom" => "ROBERT","profession" =>
-"agriculteur","age" => 45);
-$tab_personne['0'] = $tab_caracteristique_dupont;
-$tab_personne['DURAND'] = $tab_caracteristique_durand;
+  $tab = array(1=>1,3=>4,10=>12);
+
+
+
+
+
 echo "<table border=\"1\">";
 echo "<tr><td>";
 echo "Item 1";
@@ -19,20 +19,18 @@ echo "Paire / Impaire";
 echo "</td></tr>";
 
   
-foreach ($tab_personne as $cle => $val) {
-    //$cle correspond à DUPONT ou DURAND
-$nombreligne = count($val)+1; //nombre de lignes à afficher =
-//nombre d’éléments dans le tableau + en-tête
-    
-//boucle sur le tableau $val correspondant à $tab_caracteristique_dupont
-//ou $tab_caracteristique_durand
-    foreach ($val as $cle2 => $val2) {
+foreach ($tab as $cle => $val) {
+  
+$somme = $val+$cle;
+
+   
         echo "<tr>";
-        echo "<td>".$cle2."</td>";
-        echo "<td>".$val2."</td>";
-        echo "<td>"$cle2+$val2"</td>";
+        echo "<td>".$cle."</td>";
+        echo "<td>".$val."</td>";
+        echo "<td>" .$somme."</td>";
+        
         echo "</tr>";
-    }
+    
 }
 echo "</table>";
 ?>
